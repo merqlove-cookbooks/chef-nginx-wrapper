ad98845d210f1eb9cd63890798abeb7bf956862c
Alexander Merkulov <sasha@merqlove.ru>
2017-09-27 16:40:51 +0300

    bump to 6.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'sasha@merqlove.ru'
 license 'all_rights'
 description 'Installs/Configures nginx'
 long_description 'Installs/Configures nginx'
-version '1.1.1'
+version '1.2.0'
 
-depends 'chef_nginx', '6.1.1'
+depends 'chef_nginx', '~> 6.2.0'
